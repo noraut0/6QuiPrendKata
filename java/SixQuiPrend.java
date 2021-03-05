ae7ab7dcc6d7a873d27f3bea492e1fb09642d8b4
aliaume Taillandier <aliaume78@hotmail.fr>
2021-03-05 19:54:51 +0100

    1

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,25 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
 
-        return 0;
+        int result = 0;
+
+        if ( value % 10 == 0)
+        {
+            result = result + 3 ;
+        }
+        else if ( value % 5 == 0)
+        {
+            result = result + 2;
+        }
+        if ( value % 11 == 0)
+        {
+            result = result + 5;
+        }
+        if ( result == 0 ) {
+            result = 1 ;
+        }
+        return result;
     }
 
     public static void gameLoop() {
